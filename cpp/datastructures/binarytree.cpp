0f9ae93d71d6daa0192c71b609668f67043a7816
Trevor Foley <tqfoley@gmail.com>
2019-10-23 12:26:50 -0400

    Update binarytree.cpp

--- cpp/datastructures/binarytree.cpp
+++ cpp/datastructures/binarytree.cpp
@@ -2,14 +2,11 @@
 #include <iostream>
 using namespace std;
 
-
 struct Node {
 	int data;
 	struct Node *a;
 	struct Node *b;
 };
-struct Node* head = NULL;
-
 
 void insert(int new_data, struct Node **parent) {
 	if (*parent == NULL) {
@@ -22,10 +19,10 @@ void insert(int new_data, struct Node **parent) {
 	else {
 
 		if ((*parent)->data < new_data) {
-			insert(new_data, &((*parent)->a));
+			insert(new_data, &((*parent)->b));
 		}
 		else {
-			insert(new_data, &((*parent)->b));
+			insert(new_data, &((*parent)->a));
 		}
 	}
 }
@@ -51,26 +48,49 @@ bool find(int data, struct Node *parent, int *level) {
 	else {
 		(*level) = (*level) + 1;
 		if (parent->data < data) {
-			return find(data, parent->a, level);
+			return find(data, parent->b, level);
 		}
 		else {
-			return find(data, parent->b, level);
+			return find(data, parent->a, level);
 		}
 	}
 	return false;
 }
 
-void display() {
-	struct Node* ptr;
-	ptr = head;
-	while (ptr != NULL) {
-		cout << ptr->data << " ";
-		ptr = ptr->a;
+void find_lowest_common_parent(int data_a, int data_b, struct Node *parent, int * level) {
+	if (parent == NULL) {
+		return;
 	}
+	if (data_a < parent->data && data_b < parent->data) {
+		(*level)++;
+		return find_lowest_common_parent(data_a, data_b, parent->a, level);
+	}
+	if (data_a > parent->data && data_b > parent->data) {
+		(*level)++;
+		return find_lowest_common_parent(data_a, data_b, parent->b, level);
+	}
+	return;
 }
 
+int distance_between_two_leafs(int data_a, int data_b, struct Node *parent)
+{
+	int level_a = 0;
+	int level_b = 0;
+
+	if (find(data_a, parent, &level_a) && find(data_b, parent, &level_b)) {
+		int level = 0;
+		find_lowest_common_parent(data_a, data_b, parent, &level);
+
+		return level_a + level_b - 2 * level;
+	}
+
+	return -1;
+}
 
 int main() {
+
+	struct Node* head = NULL;
+
 	insert(3, &head);
 	insert(4, &head);
 	insert(8, &head);
@@ -100,6 +120,34 @@ int main() {
 	int level = 0;
 	cout << "\nfind:" << find(6, head, &level);
 	cout << "\nlevel:" << level;
+
+
+	level = 0;
+	find_lowest_common_parent(17, 27, head, &level);
+	cout << "\nlevel:" << level << "\n";
+
+	cout << "\ndistance 4 27 :" << distance_between_two_leafs(4, 27, head) << "\n";
+
+	cout << "\ndistance 4 16 :" << distance_between_two_leafs(4, 16, head) << "\n";
+
+	cout << "\ndistance 17 27 :" << distance_between_two_leafs(17, 27, head) << "\n";
+
+	cout << "\ndistance 0 4 :" << distance_between_two_leafs(4, 0, head) << "\n";
+
+	cout << "\ndistance 4 3 :" << distance_between_two_leafs(4, 3, head) << "\n";
+
+	cout << "\ndistance 16 100 :" << distance_between_two_leafs(16, 100, head) << "\n";
+
+	cout << "\ndistance 12 100 :" << distance_between_two_leafs(12, 100, head) << "\n";
+
+	cout << "\ndistance 12 100 :" << distance_between_two_leafs(12, 100, head) << "\n";
+
+	cout << "\ndistance 4 2 :" << distance_between_two_leafs(4, 2, head) << "\n";
+
+	cout << "\ndistance 16 0 :" << distance_between_two_leafs(16, 0, head) << "\n";
+
+	cout << "\ndistance 27 7 :" << distance_between_two_leafs(7, 27, head) << "\n";
+
 
 
 	return 0;
